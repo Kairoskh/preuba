e115f2637bd4b6d8cf758daab1341debf38e4dd5
Kairoskh <93041953+Kairoskh@users.noreply.github.com>
2023-12-12 18:47:13 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenfreestyle;
+
+/**
+ *
+ * @author olivert
+ */
+public class MavenFreeStyle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public int fibonacci(int n){	
+        if (n <= 1)
+        	return n;
+    	else
+        	return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
